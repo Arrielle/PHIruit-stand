80b368659ab10c5e6c525fbdd1bdb2dcd9ebcc59
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-16 11:27:52 -0600

    New Market Price

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 $(document).ready(function(){
 
-	var fruitDivArray = ['apple', 'orange', 'banana', 'grapes'];
+	var fruitDivArray = ['apple', 'orange', 'banana', 'pears', 'starfruit'];
 
 	for (var i = 0; i < fruitDivArray.length; i++) {
 		$('#' + fruitDivArray[i]).append('<button class="buyButton" id="' + fruitDivArray[i] + '" > I want buy ' + fruitDivArray[i] + ' </button>');
 	}
 
-	var centIncrement = randomNumber(1, 50) / 100;
+	var centIncrement = randomNumber(1, 50);
 	var plusOrMinus = randomNumber(0, 1);
-	var newMarketPrice = 1;
+	var newMarketPrice = randomNumber(1,9);
 	// console.log(newMarketPrice);
 
 
+for (var i = 0; i < fruitDivArray.length; i++) {
+	newMarketPrice = randomMarketPrice();
+	newMarketPrice *= 100;
+	Math.round(newMarketPrice);
+	newMarketPrice = parseInt(newMarketPrice)/100;
+	$('#' + fruitDivArray[i]).find('h3').text("New Market Price: $" + newMarketPrice);
 
-
-
-
-
-
+}
 
 
 
 	function randomMarketPrice () {
+		centIncrement = randomNumber(1,50);
 		if (newMarketPrice <= 0.5) {
-			newMarketPrice += centIncrement;
-		}else if (newMarketPrice == 9.99){
-			newMarketPrice -= centIncrement;
+			newMarketPrice += centIncrement/100;
+		}else if (newMarketPrice >= 9.99){
+			newMarketPrice -= centIncrement/100;
 		}else if (plusOrMinus == 0) {
-			newMarketPrice += centIncrement;
+			newMarketPrice += centIncrement/100;
 		}else if (plusOrMinus == 1){
-			newMarketPrice -= centIncrement;
+			newMarketPrice -= centIncrement/100;
 		}
 		return newMarketPrice;
 	}
