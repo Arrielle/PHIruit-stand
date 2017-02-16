b35db7b632a7d0055a3fc31e3e52b9f1a99416da
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-16 14:49:30 -0600

    Added average price and negative balance issue fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	var fruitDivArray = ['apple', 'orange', 'banana', 'pears', 'starfruit']; //created array of client fruit
+	var fruitDivArray = ['apple', 'orange', 'banana', 'pear']; //created array of client fruit
 
 	//for loop to append button to each fruit div on the DOM
 	for (var i = 0; i < fruitDivArray.length; i++) {
@@ -11,40 +11,124 @@ $(document).ready(function(){
 	var plusOrMinus = randomNumber(0, 1);
 	var newMarketPrice = randomNumber(1,9);
 
-	var userCash = 100; // defines starting user cash
+	var userCash = 50; // defines starting user cash
 
 	$('#userCash').text(userCash); // adding user cash new amount to DOM by changing text
 
-newRandomMarketPrice(); // calling RandomMarketPrice function
+	newRandomMarketPrice(); // calling RandomMarketPrice function
 
-setInterval(newRandomMarketPrice, 15000); //sets price to refresh every 15 seconds
+	setInterval(newRandomMarketPrice, 15000); //sets price to refresh every 15 seconds
 
+// setting each fruit's behavior for average purchase price
+	var averageApplePrice = 0;
+	var appleTotalSpent = 0;
+	var numApplesBought = 0;
 
-var averageApplePrice = 0;
-var appleTotalSpent = 0;
-var numApplesBought = 0;
+	$('#apple').on('click', 'button', function(){ // button click event listener
+		var priceAtMoment = parseFloat($(this).parent().find('h3').find('span').text());
+		userCash -= priceAtMoment; //subtracting fruit price from total cash
+		$('#userCash').text(userCash); // updating user cash on DOM;
+		numApplesBought++;
+		appleTotalSpent+=priceAtMoment;
+		averageApplePrice = appleTotalSpent/numApplesBought;
+		isUserALoser();
+		$(this).parent().find('h4').find('span').text(averageApplePrice);
 
-$('button').on('click', function(){ // button click event listener
+		$('#numApplesBought').text('You bought ' + numApplesBought  + ' apple(s)!')
+
+	}); //ends APPLE on click
+
+	var averageOrangePrice = 0;
+	var orangeTotalSpent = 0;
+	var numOrangeBought = 0;
+
+	$('#orange').on('click', 'button', function(){ // button click event listener
+		var priceAtMoment = parseFloat($(this).parent().find('h3').find('span').text());
+		userCash -= priceAtMoment; //subtracting fruit price from total cash
+		$('#userCash').text(userCash); // updating user cash on DOM
+
+		numOrangeBought++;
+		orangeTotalSpent+=priceAtMoment;
+		averageOrangePrice = orangeTotalSpent/numOrangeBought;
+		isUserALoser();
+		$(this).parent().find('h4').find('span').text(averageOrangePrice);
+
+		$('#numOrangeBought').text('You bought ' + numOrangeBought  + ' orange(s)!')
+
+	}); //ends ORANGE on click
+
+		var averageBananaPrice = 0;
+		var bananaTotalSpent = 0;
+		var numBananaBought = 0;
+
+	$('#banana').on('click', 'button', function(){ // button click event listener
+		var priceAtMoment = parseFloat($(this).parent().find('h3').find('span').text());
+		userCash -= priceAtMoment; //subtracting fruit price from total cash
+		$('#userCash').text(userCash); // updating user cash on DOM
+
+		numBananaBought++;
+		bananaTotalSpent+=priceAtMoment;
+		averageBananaPrice = bananaTotalSpent/numBananaBought;
+		isUserALoser();
+		$(this).parent().find('h4').find('span').text(averageOrangePrice);
+
+		$('#numBananaBought').text('You bought ' + numBananaBought  + ' banana(s)!')
+
+	}); //ends BANANA on click
+
+	var averagePearPrice = 0;
+	var pearTotalSpent = 0;
+	var numPearBought = 0;
+
+$('#pear').on('click', 'button', function(){ // button click event listener
 	var priceAtMoment = parseFloat($(this).parent().find('h3').find('span').text());
 	userCash -= priceAtMoment; //subtracting fruit price from total cash
 	$('#userCash').text(userCash); // updating user cash on DOM
 
-	numApplesBought++;
-	appleTotalSpent+=priceAtMoment;
+	numPearBought++;
+	pearTotalSpent+=priceAtMoment;
+	averagePearPrice = pearTotalSpent/numPearBought;
+	isUserALoser();
+	$(this).parent().find('h4').find('span').text(averagePearPrice);
 
-	averageApplePrice = appleTotalSpent/numApplesBought;
+	$('#numPearBought').text('You bought ' + numPearBought + ' pear(s)!')
 
-	console.log(averageApplePrice);
+}); //ends PEAR on click
+	//
+	// var averageStarfruitPrice = 0;
+	// var starfruitTotalSpent = 0;
+	// var numStarfruitBought = 0;
+	//
+	// $('#starfruit').on('click', 'button', function(){ // button click event listener
+	// 	var priceAtMoment = parseFloat($(this).parent().find('h3').find('span').text());
+	// 	userCash -= priceAtMoment; //subtracting fruit price from total cash
+	// 	$('#userCash').text(userCash); // updating user cash on DOM
+	//
+	// 	numStarfruitBought++;
+	// 	starfruitTotalSpent+=priceAtMoment;
+	// 	averageStarfruitPrice = starfruitTotalSpent/numStarfruitBought;
+	//
+	// 	$(this).parent().find('h4').find('span').text(averageStarfruitPrice);
+	//
+	// 	$('#numStarfruitBought').text('You bought ' + numStarfruitBought  + ' starfruit!')
+	//
+	// }); //ends STARFRUIT on click
 
-});
 
 
+	function isUserALoser(){
+		if (userCash <= newMarketPrice){
+			$('button').remove();
+			// $('button').delete();
+			console.log("user IS a loozer");
+		}
+	};
 
 	// function for parameters of new random market price to avoid errors
 	function randomMarketPrice () {
 		// newMarketPrice = randomNumber(1,9);
-		centIncrement = randomNumber(1,50);
-		if (newMarketPrice <= 0.5) {
+		centIncrement = randomNumber(1,25);
+		if (newMarketPrice <= 0.25) {
 			newMarketPrice += centIncrement/100;
 		}else if (newMarketPrice >= 9.49){
 			newMarketPrice -= centIncrement/100;
