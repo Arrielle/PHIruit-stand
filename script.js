a442d41b4bec76bf3a3fcbcecf1e05e3aed46e89
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-16 12:22:09 -0600

    added interval to market price and some styling

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
 $(document).ready(function(){
 
-	var fruitDivArray = ['apple', 'orange', 'banana', 'pears', 'starfruit'];
+	var fruitDivArray = ['apple', 'orange', 'banana', 'pears', 'starfruit']; //created array of client fruit
 
+	//for loop to append button to each fruit div on the DOM
 	for (var i = 0; i < fruitDivArray.length; i++) {
 		$('#' + fruitDivArray[i]).append('<button class="buyButton" id="' + fruitDivArray[i] + '" > I want buy ' + fruitDivArray[i] + ' </button>');
 	}
-
+	// variables declared for random number operations
 	var centIncrement = randomNumber(1, 50);
 	var plusOrMinus = randomNumber(0, 1);
 	var newMarketPrice = randomNumber(1,9);
 	// console.log(newMarketPrice);
 
+newRandomMarketPrice();
 
-for (var i = 0; i < fruitDivArray.length; i++) {
-	newMarketPrice = randomMarketPrice();
-	newMarketPrice *= 100;
-	Math.round(newMarketPrice);
-	newMarketPrice = parseInt(newMarketPrice)/100;
-	$('#' + fruitDivArray[i]).find('h3').text("New Market Price: $" + newMarketPrice);
+setInterval(newRandomMarketPrice, 1500); //sets price to refresh every 15 seconds
 
-}
-
-
-
+	// function for parameters of new random market price to avoid errors
 	function randomMarketPrice () {
+		newMarketPrice = randomNumber(1,9);
 		centIncrement = randomNumber(1,50);
 		if (newMarketPrice <= 0.5) {
 			newMarketPrice += centIncrement/100;
-		}else if (newMarketPrice >= 9.99){
+		}else if (newMarketPrice >= 9.49){
 			newMarketPrice -= centIncrement/100;
 		}else if (plusOrMinus == 0) {
 			newMarketPrice += centIncrement/100;
@@ -37,7 +32,18 @@ for (var i = 0; i < fruitDivArray.length; i++) {
 		return newMarketPrice;
 	}
 
-	// console.log(newMarketPrice);
+	// for loop to generate new random market price
+	//wrap in function yes!
+	function newRandomMarketPrice(){
+		for (var i = 0; i < fruitDivArray.length; i++) {
+			newMarketPrice = randomMarketPrice();
+			newMarketPrice *= 100;
+			Math.round(newMarketPrice);
+			newMarketPrice = parseInt(newMarketPrice)/100;
+			$('#' + fruitDivArray[i]).find('h3').text("New Market Price: $" + newMarketPrice);
+		}
+	}
+
 
 	function randomNumber(min, max) {
 		return Math.floor(Math.random() * (1 + max - min) + min);
